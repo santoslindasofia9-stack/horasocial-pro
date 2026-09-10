fc5f7e62b4ed5deace018ddbb92b85f78a97a53a
santoslindasofia9-stack <santoslindasofia9@gmail.com>
2026-09-09 22:01:18 -0500

    chore: elimina carpetas de prototipos y mockups sin uso

--- api/mensajes.js/mensajes.js
+++
@@ -1,6 +0,0 @@
-// api/mensajes.js
-const path = require('path');
-
-module.exports = (req, res) => {
-    res.sendFile(path.join(process.cwd(), 'stitch_horasocial_pro_landing_page', 'mensajer_a_y_canales_admin_horasocial_pro', 'code.html'));
-};
